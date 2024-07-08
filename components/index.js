b7db894f2c817fcb3d50730139964d83b97aa118
Aryan Thapa <aryanthapa748@gmail.com>
2024-07-08 01:05:57 -0400

    Added components folder with reusable UI components

---
+++ components/index.js
@@ -0,0 +1,9 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./button";
+
+export {
+    HomeHeader, FocusedStatusBar, NFTCard,
+    CircleButton, RectButton,
+}
