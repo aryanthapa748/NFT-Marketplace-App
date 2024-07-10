6de3f51e36aede1f80e8864050e17c0ad133ba13
Aryan Thapa <aryanthapa748@gmail.com>
2024-07-09 20:22:43 -0400

    Completed DetailsDesc and DetailsBid components for detailed views of bids

--- components/index.js
+++ components/index.js
@@ -3,7 +3,13 @@ import FocusedStatusBar from "./FocusedStatusBar";
 import NFTCard from "./NFTCard";
 import { CircleButton, RectButton } from "./button";
 
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+import { SubInfo } from "./SubInfo";
+
 export {
     HomeHeader, FocusedStatusBar, NFTCard,
     CircleButton, RectButton,
+    DetailsDesc, DetailsBid,
+    SubInfo,
 }
